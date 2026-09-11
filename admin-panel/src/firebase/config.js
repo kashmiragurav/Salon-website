f1b45cd34b6ffd23707458412c337eaa34fa66cb
kashmira gurav <kashmiragurav11@gmail.com>
2026-09-11 17:06:08 +0530

    Fix Netlify deployment configuration

--- admin-panel/src/firebase/config.js
+++ admin-panel/src/firebase/config.js
@@ -1,4 +1,4 @@
-\gitimport { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getAuth, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
